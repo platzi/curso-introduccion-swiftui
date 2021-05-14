6ea2b4cd952c46e8562802438165cbb36800c30e
Juan Villalvazo <villalvazolive@gmail.com>
2021-05-14 16:29:43 -0700

    branch computed properties

---
+++ IntroduccionSwiftUI/Playgrounds/ComputedProperties.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var suma:Int {
+    
+   10 + 8
+    
+}
+
+
+suma
+
+var multiplicacion:Int {
+    
+    let a:Int = 10 ,b:Int = 10 ,c:Int = 10
+    
+    return a*b*c
+    
+}
+
+multiplicacion
