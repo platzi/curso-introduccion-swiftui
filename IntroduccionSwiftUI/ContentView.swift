1da59b32d0ccc2be3329c562cb3df4b85363a306
Juan Villalvazo <villalvazolive@gmail.com>
2021-05-14 13:53:28 -0700

    Initial Commit

---
+++ IntroduccionSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IntroduccionSwiftUI
+//
+//  Created by Juan Villalvazo on 14/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IntroduccionSwiftUI/IntroduccionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroduccionSwiftUIApp.swift
+//  IntroduccionSwiftUI
+//
+//  Created by Juan Villalvazo on 14/05/21.
+//
+
+import SwiftUI
+
+@main
+struct IntroduccionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
