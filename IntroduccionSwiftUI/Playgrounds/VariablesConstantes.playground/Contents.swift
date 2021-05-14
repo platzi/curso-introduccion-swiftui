97ab838230a59211e38ad43e0285e7555b2045b9
Juan Villalvazo <villalvazolive@gmail.com>
2021-05-14 14:19:28 -0700

    Variables y Constantes

---
+++ IntroduccionSwiftUI/Playgrounds/VariablesConstantes.playground/Contents.swift
@@ -0,0 +1,28 @@
+
+import Foundation
+//import UIKit
+
+
+//Palabra reservada-Operador de asignacion-valor
+var greeting = "Hello, playground"
+
+var x = 10
+
+var y = 8
+
+var r = x + y
+
+var nombreVariableImportante = 2
+
+var sumaVariables = x + y
+
+//Palabra reservada-Tipo de dato-Operador de asignacion-valor
+var text:String = "Hola Mundo"
+
+var numeroVariable:Int = 18
+
+let numeroEstatico:Float = 8.9
+
+//let resultado = numeroEstatico + numeroVariable
+
+let resultado:Float = numeroEstatico + Float(numeroVariable)
