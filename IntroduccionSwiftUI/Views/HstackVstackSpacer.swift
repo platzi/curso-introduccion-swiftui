306bb7db4872887326041cb9a285788b33a18e89
Juan Villalvazo <villalvazolive@gmail.com>
2021-05-22 22:29:27 -0700

    Version final + fixes

--- IntroduccionSwiftUI/Views/HstackVstackSpacer.swift
+++ IntroduccionSwiftUI/Views/HstackVstackSpacer.swift
@@ -10,35 +10,48 @@ import SwiftUI
 struct HstackVstackSpacer: View {
     var body: some View {
         
-        VStack(alignment: .trailing) {
-            
-            //Spacer()
+        VStack(alignment: .trailing){
             
             Text("1").border(Color.black)
+            Spacer()
             
             Text("2").border(Color.black)
             
+            Spacer()
             Text("3").border(Color.black)
-           
             
-            HStack(alignment: .top) {
+            Spacer()
+            
+            HStack(alignment:.top, spacing:0){
                
-                Text("A").frame(width: 100, height: 100).border(Color.black)
-               
+                
+                Text("A").frame(width: 100, height: 100, alignment: .center).border(Color.black)
+                Spacer()
                 Text("B").border(Color.black)
+                Spacer()
                 Text("C").border(Color.black)
-               // Spacer()
-            }
-            .background(Color.red)
-           
+                Spacer()
+                
+                
+                
+                
+                
+            }.background(Color.red)
             
-           
-        }.background(Color.blue)//.ignoresSafeArea()
+            
+            
+        }.background(Color.blue)
+     
+        
+        
+        
     }
 }
 
 struct HstackVstackSpacer_Previews: PreviewProvider {
     static var previews: some View {
+        
+        Image("reto")
         HstackVstackSpacer()
     }
 }
